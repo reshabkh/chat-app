7ce6049a39cc4f845d82737b7a304f23eeafb7c3
Reshab Khandelwal <reshab>
2017-10-28 18:39:36 +0530

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,26 +26,32 @@ socket.on('newLocationMessage',function(message){
 });
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
+
+  var messageTexBox = jQuery('[name=message]')
   socket.emit('createMessage',{
     from:'User',
-    text:jQuery('[name=message]').val()
+    text:messageTexBox.val()
   },function(){
-
+    messageTexBox.val('')
   });
 });
+
 
 var locationButton = jQuery('#send-location');
 locationButton.on('click',function(){
   if(!navigator.geolocation){
     return alert('Geolocation is not supported by your browser');
   }
+  locationButton.attr('disabled','disabled').text('sending location.....');
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('send location');
     socket.emit('createLocationMessage',{
       latitude : position.coords.latitude,
       longitude :position.coords.longitude
     });
 
   },function(){
+      locationButton.removeAttr('disabled').text('send location');
     alert('unable to fetch location');
   });
 });
