c4b014c1784b21eaeabafa9905ee5bb1e647df1e
Reshab Khandelwal <reshab>
2017-11-07 13:26:39 +0530

    Send message to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -69,7 +69,7 @@ jQuery('#message-form').on('submit',function(e){
 
   var messageTextBox = jQuery('[name=message]')
   socket.emit('createMessage',{
-    from:'User',
+
     text:messageTextBox.val()
   },function(){
     messageTextBox.val('')
